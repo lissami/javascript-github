79b8bd163076bc4b6258ce3107de645a3a376cdb
lissami <taeginational@gmail.com>
2025-01-28 15:48:30 +0300

    changes to js

--- homework.js
+++ homework.js
@@ -1,1 +1,14 @@
-console.log('hello')
+const myCity = 'Kostroma'
+
+const weather = {
+    temperature: 3,
+    windSpeed: 18
+};
+
+console.log('Мой город:', myCity);
+console.log('Температура в городе:', weather.temperature);
+console.log('Скорость ветра:', weather.windSpeed);
+
+const result = ((20 + 5) * 3) // Это выражение
+
+console.log(result) // Это показывает результат выражения выше
